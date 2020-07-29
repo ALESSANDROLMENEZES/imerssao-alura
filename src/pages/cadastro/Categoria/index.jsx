59b31e4aea22a65cca0f9efb4afe8505ff3b9824
Alessandro <alessandroadm@live.com>
2020-07-28 21:57:24 -0300

    feact(index): edding class 2

--- src/pages/cadastro/Categoria/index.jsx
+++ src/pages/cadastro/Categoria/index.jsx
@@ -5,6 +5,15 @@ import {Link} from 'react-router-dom';
 export default props => (
   <PageDefault>
     <h1>Cadastro de categoria</h1>
+
+    <form>
+      <label>Nome da categoria
+        <input type="text" />
+      </label>
+
+      <button>Cadastrar</button>
+    </form>
+
     <Link to="/">Ir para Home</Link>
   </PageDefault>
 );
